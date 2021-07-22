f44f600417fdaf5f2fb1f37c7b2bd87c90cfb1cb
Warren <wvandentop@drummondco.com>
2021-07-22 15:42:04 -0500

    module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,7 @@ class Quiz extends Component{
 		const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
 		return (
 		  <div>
-		  {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler = {this.showNextQuestion.bind(this)} />}
+		  {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler = {this.showNextQuestion.bind(this)} />}
 		  </div>
 		)
 	}
@@ -22,6 +22,13 @@ class Quiz extends Component{
 	showNextQuestion() {
 		this.setState((state) => {
 			return {quiz_position: state.quiz_position + 1}
+		})
+	}
+	
+	handleResetClick() {
+		this.props.resetClickHandler()
+		this.setState((state) => {
+			return {quiz_position: 1}
 		})
 	}
 }
