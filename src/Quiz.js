39479feebd121d9781e88981e8b5fbe96a230c6b
Warren <wvandentop@drummondco.com>
2021-07-22 11:09:55 -0500

    module 3

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,9 +11,7 @@ class Quiz extends Component{
 	render() {
 		return (
 		  <div>
-			<div className="QuizQuestion">
-				{quizData.quiz_questions[0].instruction_text}
-			</div>
+			<QuizQuestion />
 		  </div>
 		)
 	}
